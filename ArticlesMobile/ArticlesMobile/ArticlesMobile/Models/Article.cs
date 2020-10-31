51b3437526e3f6bedbeab75a5dd7206d026d4024
Luis Esquivel <luisesquivel7f5@gmail.com>
2020-10-31 13:29:03 -0600

    MODELO DE ATRICULO

---
+++ ArticlesMobile/ArticlesMobile/ArticlesMobile/Models/Article.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArticlesMobile.Models
+{
+    public class Article
+    {
+
+        public int _Id { get; set; }
+
+        public DateTime date { get; set; }
+
+        public string title { get; set; }
+
+        public string content { get; set; }
+
+        public string image { get; set; }
+
+    }
+}
